8d98c104b43e0181459b532f4464e27ffea82c87
Joshua Cox <joshua.n.cox@gmail.com>
2023-11-13 21:55:05 -0800

    Remove outdated commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,5 @@
-import ping from './ping';
-import server from './server';
-import user from './user';
-import joke from './joke';
 import ask from './ai/ask';
 import model from './ai/model';
 import stream from './ai/ask-stream';
 
-export const commands = { ping, server, user, joke, ask, model, stream };
+export const commands = { ask, model, stream };
